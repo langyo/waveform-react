b0c83e741b13623d9e480e5357cf01209a32e1f0
langyo <langyo.china@gmail.com>
2021-05-24 22:30:00 +0800

    🔥 将一切与主要功能无关的模块全部移除。

--- examples/src/components/App.js
+++ examples/src/components/App.js
@@ -1,53 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import Button from './Button';
-import Checkbox from './Checkbox';
-import ColorInput from './ColorInput';
-import FileInput from './FileInput';
-import NumberInput from './NumberInput';
-import Title from './Title';
 import Waveform from '../lib';
 import { getAudioBuffer, getContext } from './utils';
-import { color } from '../styles/theme';
-
-const Columns = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const Column = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-`;
-
-const Heading = styled.div`
-  font-weight: 600;
-  padding-right: 20px;
-`;
-
-const InputGroup = styled.div`
-  align-items: center;
-  display: flex;
-  margin: 10px 0;
-`;
-
-const WaveformWrapper = styled.div`
-  height: 300px;
-  width: 100%;
-`;
-
-const Wrapper = styled.div`
-  background: linear-gradient(
-    to bottom,
-    ${p => p.theme.color.background},
-    ${p => p.theme.color.backgroundSecondary}
-  );
-  width: 100%;
-  height: 100vh;
-  margin: 0;
-  padding: 0.8em;
-`;
 
 class App extends React.PureComponent {
   state = {
@@ -55,15 +8,15 @@ class App extends React.PureComponent {
     context: null,
     height: 150,
     markerStyle: {
-      color: color.tertiary,
+      color: '#666',
       width: 4
     },
     position: 0,
     responsive: true,
     showPosition: true,
     waveStyle: {
-      animate: true,
-      color: color.primary,
+      animate: false,
+      color: '#999',
       plot: 'bar',
       pointWidth: 1
     },
@@ -77,15 +30,11 @@ class App extends React.PureComponent {
     });
   }
 
-  getFile = async (path = 'audio/test.mp3') => {
-    const buffer = await getAudioBuffer(path, this.state.context);
-    this.setState({ buffer });
-  };
-
-  handleFile = event => {
+  handleFile = async event => {
     const files = event.target.files;
-    const file = window.URL.createObjectURL(files[0]);
-    this.getFile(file);
+    const filePath = window.URL.createObjectURL(files[0]);
+    const buffer = await getAudioBuffer(filePath, this.state.context);
+    this.setState({ buffer });
   };
 
   setValue = (val, prop, sub) => {
@@ -116,95 +65,26 @@ class App extends React.PureComponent {
 
   render() {
     return (
-      <Wrapper>
-        <Title>Waveform</Title>
-        <Button onClick={() => this.getFile()}>Example File</Button>
-        <FileInput accept="audio/*" onChange={this.handleFile} />
-        <Columns>
-          <Column>
-            <InputGroup>
-              <Heading>Wave Color</Heading>
-              <ColorInput
-                color={this.state.waveStyle.color}
-                onChange={e => this.setValue(e.hex, 'color', 'waveStyle')}
-              />
-            </InputGroup>
-            <InputGroup>
-              <Heading>Point Width</Heading>
-              <NumberInput
-                max={20}
-                onChange={e => this.setValue(e, 'pointWidth', 'waveStyle')}
-                value={this.state.waveStyle.pointWidth}
-              />
-            </InputGroup>
-            <InputGroup>
-              <Heading>Animate</Heading>
-              <Checkbox
-                onChange={e =>
-                  this.setValue(e.target.checked, 'animate', 'waveStyle')
-                }
-                value={this.state.waveStyle.animate}
-              />
-            </InputGroup>
-            <InputGroup>
-              <Heading>Responsive</Heading>
-              <Checkbox
-                onChange={e => this.setValue(e.target.checked, 'responsive')}
-                value={this.state.responsive}
-              />
-            </InputGroup>
-            <InputGroup>
-              <Heading>Plot Type (checked = 'line')</Heading>
-              <Checkbox
-                onChange={e =>
-                  this.setValue(
-                    e.target.checked ? 'line' : 'bar',
-                    'plot',
-                    'waveStyle'
-                  )
-                }
-                value={this.state.waveStyle.plot === 'line'}
-              />
-            </InputGroup>
-          </Column>
-          <Column>
-            <InputGroup>
-              <Heading>Show Position Marker</Heading>
-              <Checkbox
-                onChange={e => this.setValue(e.target.checked, 'showPosition')}
-                value={this.state.showPosition}
-              />
-            </InputGroup>
-            <InputGroup>
-              <Heading>Marker Width</Heading>
-              <NumberInput
-                onChange={e => this.setValue(e, 'width', 'markerStyle')}
-                value={this.state.markerStyle.width}
-              />
-            </InputGroup>
-            <InputGroup>
-              <Heading>Marker Color</Heading>
-              <ColorInput
-                color={this.state.markerStyle.color}
-                onChange={e => this.setValue(e.hex, 'color', 'markerStyle')}
-              />
-            </InputGroup>
-          </Column>
-        </Columns>
-        <WaveformWrapper>
-          <Waveform
-            buffer={this.state.buffer}
-            height={this.state.height}
-            markerStyle={this.state.markerStyle}
-            onPositionChange={pos => this.setValue(pos, 'position')}
-            position={this.state.position}
-            responsive={this.state.responsive}
-            showPosition={this.state.showPosition}
-            waveStyle={this.state.waveStyle}
-            width={this.state.width}
-          />
-        </WaveformWrapper>
-      </Wrapper>
+      <div>
+        <input
+          accept="audio/*"
+          type="file"
+          name="file"
+          id="file"
+          onChange={this.handleFile}
+        />
+        <Waveform
+          buffer={this.state.buffer}
+          height={200}
+          markerStyle={{ color: '#333' }}
+          onPositionChange={pos => this.setValue(pos, 'position')}
+          position={this.state.position}
+          responsive={this.state.responsive}
+          showPosition={this.state.showPosition}
+          waveStyle={this.state.waveStyle}
+          width={800}
+        />
+      </div>
     );
   }
 }
--- examples/src/index.js
+++ examples/src/index.js
@@ -1,17 +1,8 @@
 import 'babel-polyfill';
-import './styles/global';
 import React from 'react';
 import { render } from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 import App from './components/App';
-
-import theme from './styles/theme';
 
 const appElement = document.getElementById('root');
 
-render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  appElement
-);
+render(<App />, appElement);
